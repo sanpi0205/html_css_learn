3c3d1137e6e4a764551e0626d9d4fc68a75d06b5
ZhangBo <sanpi0205@gmail.com>
2014-08-12 09:39:24 +0800

    create my css learn example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HtmlCssLearn::Application.config.secret_key_base = '29f89132ff438c0259beff94e70fd4e5563ae636b2294b44ca6adfe15caab641271c91150bfc359a8e68d151eeee85f87ddd8154a30f7c982144299496bba322'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HtmlCssLearn::Application.config.session_store :cookie_store, key: '_html_css_learn_session'
